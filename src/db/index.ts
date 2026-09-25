3943255fc804df5901bd220ef657bc5e84a93d8a
saigampala <saiphanindra.gampala@gmail.com>
2026-09-25 17:33:33 +0530

    deployment error

--- src/db/index.ts
+++ src/db/index.ts
@@ -15,6 +15,9 @@ export const pool =
   globalForDb.__arenaNextJsPostgresqlPool ??
   new Pool({
     connectionString: databaseUrl,
+    ssl: databaseUrl.includes("sslmode=require")
+      ? { rejectUnauthorized: false }
+      : false,
   });
 
 if (process.env.NODE_ENV !== "production") {
